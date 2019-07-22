9b19a1e045b442635fea6140a9f18ab18fcc7602
Gabriel Chiquini <gabrielsantos@tilix.com.br>
2019-07-22 15:46:00 -0300

    Changed to sparse matrix using partial pivoting with natural ordering, tests passing

--- spec/Calculator/Solve.spec.ts
+++ spec/Calculator/Solve.spec.ts
@@ -1,10 +1,10 @@
-import { bignumber, BigNumber } from 'mathjs';
+import {bignumber, BigNumber} from 'mathjs';
 import solve from '../../src/Calculator/Solve';
-import { ICircuitRepresentation } from '../../src/Deserialize/CircuitRepresentation';
+import {ICircuitRepresentation} from '../../src/Deserialize/CircuitRepresentation';
 import deserialize from '../../src/Deserialize/Deserializer';
-import { EXAMPLE1 } from '../Examples/valid/Example1';
-import { EXAMPLE2 } from '../Examples/valid/Example2';
-import { EXAMPLE3 } from '../Examples/valid/Example3';
+import {EXAMPLE1} from '../Examples/valid/Example1';
+import {EXAMPLE2} from '../Examples/valid/Example2';
+import {EXAMPLE3} from '../Examples/valid/Example3';
 
 describe('Matrices generation', () => {
   it('Should solve example 1', () => {
@@ -19,12 +19,24 @@ describe('Matrices generation', () => {
   });
 });
 
-function testExample(example: ICircuitRepresentation, expected: Array<number | BigNumber>) {
-  const circuit = deserialize(example);
-  const matrix = solve(circuit);
-  expect(matrix).toEqual(mapToBigNumber(expected));
+function mapToNumber(matrix: Array<number | BigNumber>): number[] {
+  return matrix.map(value => {
+    if (typeof value === 'number') {
+      return value;
+    } else {
+      return value.toNumber();
+    }
+  });
 }
 
-function mapToBigNumber(expected: Array<number | BigNumber>): BigNumber[] {
-  return expected.map(value => bignumber(value)) as BigNumber[];
+function testExample(example: ICircuitRepresentation, expected: Array<number | BigNumber>) {
+  const circuit = deserialize(example);
+  console.time('solve');
+  const matrix = solve(circuit);
+  console.timeEnd('solve');
+  const expectedNumber = mapToNumber(expected);
+  const matrixNumber = mapToNumber(matrix);
+  for (let i = 0; i < matrix.length; i++) {
+    expect(matrixNumber[i]).toBeCloseTo(expectedNumber[i], 0.0001);
+  }
 }
--- src/Calculator/Solve.ts
+++ src/Calculator/Solve.ts
@@ -1,4 +1,4 @@
-import { BigNumber, lusolve } from 'mathjs';
+import {BigNumber, lusolve, Matrix, sparse} from 'mathjs';
 import Circuit from '../Circuit/Circuit';
 import conductanceMatrix from './ConductanceMatrix';
 import sourceMatrix from './SourceMatrix';
@@ -6,8 +6,8 @@ import sourceMatrix from './SourceMatrix';
 export default function solve(circuit: Circuit): BigNumber[] {
   const cMatrix = conductanceMatrix(circuit);
   const sMatrix = sourceMatrix(circuit);
-
-  return (lusolve(cMatrix as any, sMatrix as any) as any[])
+  const sparseMatrix = sparse(cMatrix as any);
+  return ((lusolve(sparseMatrix as any, sMatrix as any, 0, 0.00001) as Matrix).toArray() as any[])
     .map((arr: BigNumber[]) => arr[0])
     .slice(0, circuit.nodePoles.length);
 }
